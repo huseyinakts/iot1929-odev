37e06a5d9b8bd60d2cfe9c5afa7de3a971b6e954
huseyinakts <58725273+huseyinakts@users.noreply.github.com>
2021-03-31 19:03:40 +0300

    Add files via upload

---
+++ karasimsek/karasimsek.ino
@@ -0,0 +1,32 @@
+void setup() {
+  
+  pinMode (2 , OUTPUT);
+  pinMode (3 , OUTPUT);
+  pinMode (4 , OUTPUT);
+  pinMode (5 , OUTPUT);
+  pinMode (6 , OUTPUT);
+}
+
+void loop() {
+ 
+  digitalWrite(6,1);
+  delay(500); 
+  digitalWrite(5,1);
+  delay(500); 
+  digitalWrite(4,1);
+  delay(500); 
+  digitalWrite(3,1);
+  delay(500);
+  digitalWrite(2,1);
+  delay(500);
+  digitalWrite(2,0);
+  delay(500);
+  digitalWrite(3,0);
+  delay(500);
+  digitalWrite(4,0);
+  delay(500);
+  digitalWrite(5,0);
+  delay(500);
+  digitalWrite(6,0);
+  delay(500);
+}
